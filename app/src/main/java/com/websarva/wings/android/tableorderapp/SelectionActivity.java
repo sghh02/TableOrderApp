b279f52abdea7e6090015de2b5a3e6d99c51318c
kaito0902 <66310212+kaito0902@users.noreply.github.com>
2022-06-06 23:21:20 +0900

    管理者画面、お客様画面への遷移Activityの追加 OrderListingFragmentのデザイン変更 MenuEditActivityの追加

---
+++ app/src/main/java/com/websarva/wings/android/tableorderapp/SelectionActivity.java
@@ -0,0 +1,35 @@
+package com.websarva.wings.android.tableorderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.websarva.wings.android.tableorderapp.administrator.MenuEditActivity;
+import com.websarva.wings.android.tableorderapp.customer.CustomerMenuActivity;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
+
+        // 管理者画面への遷移ボタンIDを取得
+        Button button_administrator = findViewById(R.id.btAdministrator);
+
+        // お客様画面への遷移ボタンIDを取得
+        Button button_customer = findViewById(R.id.btCustomer);
+
+        button_administrator.setOnClickListener(view -> {
+            Intent intent = new Intent(SelectionActivity.this, MenuEditActivity.class);
+            startActivity(intent);
+        });
+
+        button_customer.setOnClickListener(view -> {
+            Intent intent = new Intent(SelectionActivity.this, CustomerMenuActivity.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/websarva/wings/android/tableorderapp/customer/PagerAdapter.java
@@ -0,0 +1,36 @@
+package com.websarva.wings.android.tableorderapp.customer;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class PagerAdapter extends FragmentStateAdapter {
+
+    public PagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return new TabFragment2();
+            case 2:
+                return new TabFragment3();
+            case 3:
+                return new TabFragment4();
+            case 4:
+                return new TabFragment5();
+            default:
+                return new TabFragment1();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+}
