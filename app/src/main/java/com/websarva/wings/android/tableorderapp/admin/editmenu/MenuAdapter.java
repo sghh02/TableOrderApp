e45333f490e03a0c25d1916e8942c824511bfb11
kaito0902 <66310212+kaito0902@users.noreply.github.com>
2022-06-30 07:55:18 +0900

    RecyclerViewの実装

--- app/src/main/java/com/websarva/wings/android/tableorderapp/admin/editmenu/MenuAdapter.java
+++ app/src/main/java/com/websarva/wings/android/tableorderapp/admin/editmenu/MenuAdapter.java
@@ -1,13 +1,10 @@
 package com.websarva.wings.android.tableorderapp.admin.editmenu;
 
 import android.annotation.SuppressLint;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -15,30 +12,39 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.websarva.wings.android.tableorderapp.R;
+import com.websarva.wings.android.tableorderapp.admin.editmenu.fragment.EditFragment1;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
 
-    private final String[] localDataSet;
+    private final ArrayList<String> localDataSet;
+    private EditFragment1 editFragment1;
 
     /**
      * 使用しているビューの種類への参照を提供します。
      * (カスタムViewHolder)を使用します。
      */
     static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        // ビューに配置されたウィジェットへの参照を保持しておくためのフィールド
+        private final TextView textView; // リストの内容
+        public ImageButton mbtMove;      // 移動ボタン
+        public ImageButton mbtDel;       // 削除ボタン
 
         public ViewHolder(View view) {
             super(view);
+
+            // 各項目の参照を取得
             textView = view.findViewById(R.id.edit_text_view);
+            mbtMove = view.findViewById(R.id.btn_move);
+            mbtDel = view.findViewById(R.id.btn_del);
         }
 
         public TextView getTextView() {
             return textView;
         }
     }
-    public MenuAdapter(String[] dataset) {
+    public MenuAdapter(ArrayList<String> dataset) {
         localDataSet = dataset;
     }
 
@@ -55,18 +61,51 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // 2.onCreateViewHolder()で、アイテムのレイアウトXMLファイルをもとにビューホルダーを作成して返す。
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_menu, parent, false);
+
+        // EditFragment1を取得
+//        editFragment1.getParentFragmentManager();
+
+        return new ViewHolder(view);
     }
 
     // 3.onBindViewHolder()を呼び出す。そのときに、２で作成したビューホルダーと表示される位置を受け取る。
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         // 4.受け取ったビューホルダーに表示するデータを埋め込む
-        viewHolder.getTextView().setText(localDataSet[position]);
+        viewHolder.getTextView().setText(localDataSet.get(position));
+
+        // 移動ボタンをタッチ
+        viewHolder.mbtMove.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+                    // 長押しでなく、タッチしてすぐにドラッグ状態にする。
+                    editFragment1.itemTouchHelper.startDrag(viewHolder);
+                    return true;
+                }
+                return v.onTouchEvent(event);
+            }
+        });
+
+        // 削除ボタンをクリック
+        viewHolder.mbtDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = viewHolder.getAdapterPosition();
+                if (adapterPosition != -1) {
+                    localDataSet.remove(adapterPosition);
+                    notifyItemRemoved(adapterPosition);
+                }
+            }
+        });
     }
 
+    // アイテム数を取得
     @Override
     public int getItemCount() {
-        return localDataSet.length;
+        return localDataSet.size();
     }
 }
--- app/src/main/java/com/websarva/wings/android/tableorderapp/admin/editmenu/fragment/EditFragment1.java
+++ app/src/main/java/com/websarva/wings/android/tableorderapp/admin/editmenu/fragment/EditFragment1.java
@@ -5,20 +5,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.websarva.wings.android.tableorderapp.R;
 import com.websarva.wings.android.tableorderapp.admin.editmenu.MenuAdapter;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class EditFragment1 extends Fragment {
 
     private static final String TAG = Object.class.getName();
+    public ItemTouchHelper itemTouchHelper;
+    private View mview;
 
     ImageButton add_menu_button1;
     ImageButton add_menu_button2;
@@ -39,28 +45,60 @@ public class EditFragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: called");
+        mview = inflater.inflate(R.layout.fragment_edit1, container, false);
 
         // ---> RecyclerView_start
         // RecyclerViewの取得
-        recyclerView1 = (RecyclerView) getView().findViewById(R.id.edit_list1);
-        recyclerView2 = (RecyclerView) getView().findViewById(R.id.edit_list2);
-        recyclerView3 = (RecyclerView) getView().findViewById(R.id.edit_list3);
-        recyclerView4 = (RecyclerView) getView().findViewById(R.id.edit_list4);
+        recyclerView1 = mview.findViewById(R.id.edit_list1);
+        recyclerView2 = mview.findViewById(R.id.edit_list2);
+        recyclerView3 = mview.findViewById(R.id.edit_list3);
+        recyclerView4 = mview.findViewById(R.id.edit_list4);
 
         //Adapterの設定
-        String[] data = new String[20];
-        for (int i =0; i <= 20; i++) {
-            data[i] = "test" + i;
+        ArrayList<String> data = new ArrayList<>();
+        for (int i =1; i <= 20; i++) {
+            data.add("test" + i);
         }
-        Adapter adapter = (Adapter) new MenuAdapter(data);
-        recyclerView1.setAdapter((RecyclerView.Adapter) adapter);
+
+        MenuAdapter adapter = new MenuAdapter(data);
+        recyclerView1.setAdapter(adapter);
 
         //LayoutManagerの設定
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView1.setLayoutManager(layoutManager);
-         // ---> RecyclerView_end
+        // ---> RecyclerView_end
 
-        return inflater.inflate(R.layout.fragment_edit1, container, false);
+        // ドラッグアンドドロップで移動
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN,
+                        ItemTouchHelper.LEFT) {
+                    // 長押しで移動
+                    @Override
+                    public boolean onMove(@NonNull RecyclerView recyclerView,
+                                          @NonNull RecyclerView.ViewHolder viewHolder,
+                                          @NonNull RecyclerView.ViewHolder target) {
+                        final int fromPos = viewHolder.getAdapterPosition();
+                        final int toPos = target.getAdapterPosition();
+                        // データを入れ替え
+                        Collections.swap(data, fromPos, toPos);
+                        // 移動したことを通知
+                        adapter.notifyItemMoved(fromPos, toPos);
+                        return true;
+                    }
+
+                    // スワイプで削除
+                    @Override
+                    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                        // アイテムを削除
+                        data.remove(viewHolder.getAdapterPosition());
+                        // 削除したことを通知
+                        adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+                    }
+                });
+        // ItemTouchHelper を RecyclerView にアタッチ
+        itemTouchHelper.attachToRecyclerView(recyclerView1);
+
+        return mview;
     }
 
     @Override
@@ -70,6 +108,7 @@ public class EditFragment1 extends Fragment {
 
         // add_menu_button1を取得
         add_menu_button1 = (ImageButton) getView().findViewById(R.id.add_menu_button1);
+        // add_menu_button1を押下時の処理
         add_menu_button1.setOnClickListener(v ->
                 Toast.makeText(getActivity(), "Button1を押した！", Toast.LENGTH_SHORT).show());
 
