2890d65389b40ea03cf930ea0d3179ae236beddf
kaito0902 <66310212+kaito0902@users.noreply.github.com>
2022-07-14 00:28:50 +0900

    ContentProviderの実装

--- app/src/main/java/com/websarva/wings/android/tableorderapp/database/DBContract.java
+++ app/src/main/java/com/websarva/wings/android/tableorderapp/database/DBContract.java
@@ -1,6 +1,9 @@
 package com.websarva.wings.android.tableorderapp.database;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
+
+import com.websarva.wings.android.tableorderapp.provider.ProductProvider;
 
 // データベースのテーブル名・項目名を定義
 public final class DBContract {
@@ -10,6 +13,17 @@ public final class DBContract {
 
     // テーブルの内容を定義
     public static class ProductEntry implements BaseColumns {
+
+        // URIパス
+        public static final String PATH = "product";
+        // コンテントURI
+        public static final Uri CONTENT_URI = Uri.parse("content://" + ProductProvider.AUTHORITY + "/" + PATH);
+        // テーブル指定コンテントタイプ
+        public static final String CONTENT_TYPE = "vnd.android.cursor.item/vnd.example.products";
+        // レコード個別指定コンテントタイプ
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.dir/vnd.example.products";
+
+
         // BaseColumns インターフェースを実装することで、内部クラスは_IDを継承できる
         public static final String PRODUCT_TABLE_NAME = "product";
         public static final String COLUMN_NAME_PRODUCT_ID = "product_id";
--- app/src/main/java/com/websarva/wings/android/tableorderapp/provider/ProductProvider.java
+++ app/src/main/java/com/websarva/wings/android/tableorderapp/provider/ProductProvider.java
@@ -1,45 +1,132 @@
 package com.websarva.wings.android.tableorderapp.provider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.websarva.wings.android.tableorderapp.database.DBContract.ProductEntry;
+import com.websarva.wings.android.tableorderapp.database.ProductOpenHelper;
+
 public class ProductProvider extends ContentProvider {
 
-    @Override
-    public boolean onCreate() {
-        return false;
+    // Authority
+    public static final String AUTHORITY = "com.websarva.wings.android.tableorderapp.provider";
+
+    // Productテーブル URI ID
+    private static final int PRODUCTS = 1;
+    // Productテーブル 個別 URI ID
+    private static final int PRODUCT_ID = 2;
+
+    // 利用者がメソッドを呼び出したURIに対応する処理を判定処理に使用する
+    private static UriMatcher sUriMatcher;
+    static {
+        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        sUriMatcher.addURI(AUTHORITY, ProductEntry.PATH, PRODUCTS);
+        sUriMatcher.addURI(AUTHORITY, ProductEntry.PATH + "/#", PRODUCT_ID);
     }
 
+    // DBHelperのインスタンス
+    private ProductOpenHelper mProductOpenHelper;
+
+    // コンテンツプロバイダの作成
+    @Override
+    public boolean onCreate() {
+        mProductOpenHelper = new ProductOpenHelper(getContext());
+        return true;
+    }
+
+    // query実行
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        switch (sUriMatcher.match(uri)) {
+            case PRODUCTS:
+            case PRODUCT_ID:
+                queryBuilder.setTables(ProductEntry.PRODUCT_TABLE_NAME);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        SQLiteDatabase db = mProductOpenHelper.getReadableDatabase();
+        Cursor cursor = queryBuilder.query(db, strings, s, strings1, null, null, s1);
+        return cursor;
     }
 
-    @Nullable
-    @Override
-    public String getType(@NonNull Uri uri) {
-        return null;
-    }
-
+    // insert実行
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+        String insertTable;
+        Uri contentUri;
+        switch (sUriMatcher.match(uri)) {
+            case PRODUCTS:
+                insertTable = ProductEntry.PRODUCT_TABLE_NAME;
+                contentUri = ProductEntry.CONTENT_URI;
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        SQLiteDatabase db = mProductOpenHelper.getWritableDatabase();
+        long rowId = db.insert(insertTable, null, contentValues);
+        if (rowId > 0) {
+            Uri returnUri = ContentUris.withAppendedId(contentUri, rowId);
+            getContext().getContentResolver().notifyChange(returnUri, null);
+            return returnUri;
+        } else {
+            throw new IllegalArgumentException("Failed to insert row into " + uri);
+        }
     }
 
+    // delete実行
     @Override
     public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+        SQLiteDatabase db = mProductOpenHelper.getWritableDatabase();
+        int count;
+        switch (sUriMatcher.match(uri)) {
+            case PRODUCTS:
+            case PRODUCT_ID:
+                count = db.delete(ProductEntry.PRODUCT_TABLE_NAME, s, strings);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
     }
 
+    // update実行
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+        SQLiteDatabase db = mProductOpenHelper.getWritableDatabase();
+        int count;
+        String id = uri.getPathSegments().get(1);
+        count = db.update(ProductEntry.PRODUCT_TABLE_NAME, contentValues, s, strings);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    // コンテントタイプ取得
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        switch (sUriMatcher.match(uri)) {
+            case PRODUCTS:
+                return ProductEntry.CONTENT_TYPE;
+            case PRODUCT_ID:
+                return ProductEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
     }
 }
